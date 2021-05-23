db86403f8d70ada82fc868b2143a72565fc0aa57
ejelum <70863699+ejelum@users.noreply.github.com>
2021-05-23 21:54:56 +0200

    Create java.js

---
+++ img/js/java.js
@@ -0,0 +1,13 @@
+const daughter = {
+  name: "Oghenevovwero",
+  age: 6,
+  height: 73,
+  parent: {
+    dad: "ejiroelum",
+    mum: "ijeomaelum",
+  },
+  parentupdate: function (daddy, mummy) {
+    this.parent.dad = daddy;
+    this.parent.mmum = mummy;
+  },
+};
