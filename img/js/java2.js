49dddf3802ffb24eb583e4b485747fe6bb165340
ejelum <70863699+ejelum@users.noreply.github.com>
2021-05-23 21:54:27 +0200

    Create java2.js

---
+++ img/js/java2.js
@@ -0,0 +1,10 @@
+import daughter from "js/java1.js";
+const anotherdaughter = new daughter (
+  "monique",
+  2,
+  "female",
+  45,
+  "ejiroelum",
+  "ijeomaelum",
+  "Julius weber Str 12 47053 Duisburg"
+);
