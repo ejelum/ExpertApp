e3817b150c5dfeed8a67faec1860130cad81f158
ejelum <70863699+ejelum@users.noreply.github.com>
2021-05-23 21:54:49 +0200

    Create java1.js

---
+++ img/js/java1.js
@@ -0,0 +1,26 @@
+class daughter {
+  constructor (
+    name,
+    age,
+    sex,
+    height,
+    mum,
+    dad,
+    address,
+    lidOpen
+  ) {
+    this.name = Oghenevovwero;
+    this.age = 6;
+    this.sex = "female";
+    this.height = 72;
+    this.parent = {
+      ejiroelum: dad,
+      ijeomaelum: mum,
+    };
+    this.lidOpen = lidOpen;
+    this.address = "Julius weber Str 12 47053 Duisburg";
+  }
+toggleLid (lidStatus) {
+  this.lidOpen = lidStatus;
+}
+}
